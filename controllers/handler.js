c25e0d5729610d761505b23b6874a581aafc0bd1
weidni96 <viechers@web.de>
2017-01-13 14:01:52 +0100

    new commit

--- controllers/handler.js
+++ controllers/handler.js
@@ -27,7 +27,7 @@ module.exports = {
   deleteItem: function(request, reply) {
     try {
       if (!users.hasOwnProperty(request.params.user))
-        reply(boom.notFound())
+        reply(boom.notFound());
       else if (!users[request.params.user].hasOwnProperty(request.params.item))
                   reply(boom.notFound());
       else {
